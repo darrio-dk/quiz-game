0f4458ed1b6b861598b1f28731f7f9e4e0ca193f
darrio-dk <darrio.dk@gmail.com>
2023-03-23 20:06:19 +0000

    timer extended, add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//  Questions added for quiz game
 const QuizData = [
     {
         "question": "Q1: What is the largest country in the world (by land area)?",
@@ -42,24 +43,25 @@ const QuizData = [
         "ans": "ans4"
     }
     
-]
+];
 // 
 
 
-const startTest = document.querySelector('#startTest')
+const startTest = document.querySelector('#startTest');
 
 
 startTest.addEventListener('click', () => {
-    document.querySelector('.startPage').style.display = 'none'
-})
+    document.querySelector('.startPage').style.display = 'none';
+});
 
+// Add timer for all the questions
 
 const timer = document.getElementById("timer");
 let timerInterval;
 
 startTimer = () => {
     clearInterval(timerInterval);
-    let second = 0,
+    let second = 30,
         minute = 1;
 
     timerInterval = setInterval(function () {
@@ -81,73 +83,73 @@ startTimer = () => {
 };
 
 
+// Constants declared for DOM elements and possible choices
 
 
+const quiz = document.querySelector('.quiz');
 
-const quiz = document.querySelector('.quiz')
+const question = document.querySelector('.question');
 
-const question = document.querySelector('.question')
+const option1 = document.querySelector('.option1');
+const option2 = document.querySelector('.option2');
+const option3 = document.querySelector('.option3');
+const option4 = document.querySelector('.option4');
 
-const option1 = document.querySelector('.option1')
-const option2 = document.querySelector('.option2')
-const option3 = document.querySelector('.option3')
-const option4 = document.querySelector('.option4')
-
-const answers = document.querySelectorAll('.answer')
-const submit = document.querySelector('#submit')
+const answers = document.querySelectorAll('.answer');
+const submit = document.querySelector('#submit');
 
 
 let questionCount = 0;
 let score = 0;
 
 const loadQuestion = () => {
-    question.innerHTML = QuizData[questionCount].question
-    option1.innerHTML = QuizData[questionCount].a
-    option2.innerHTML = QuizData[questionCount].b
-    option3.innerHTML = QuizData[questionCount].c
-    option4.innerHTML = QuizData[questionCount].d
-}
+    question.innerHTML = QuizData[questionCount].question;
+    option1.innerHTML = QuizData[questionCount].a;
+    option2.innerHTML = QuizData[questionCount].b;
+    option3.innerHTML = QuizData[questionCount].c;
+    option4.innerHTML = QuizData[questionCount].d;
+};
 
-loadQuestion()
+loadQuestion();
 
-
+// Add event listener to answer and score
 
 submit.addEventListener('click', () => {
 
     const CheckAnswer = () => {
-        let answer
+        let answer;
         answers.forEach(cur => {
             if (cur.checked) {
-                answer = cur.id
+                answer = cur.id;
             }
-        })
-        return answer
-    }
+        });
+        return answer;
+    };
     if (CheckAnswer() === QuizData[questionCount].ans) {
-        score++
+        score++;
     }
 
 
     if(questionCount < QuizData.length - 2){
-        submit.innerHTML = '<p>Next</p>'
+        submit.innerHTML = '<p>Next</p>';
     } else {
-        submit.innerHTML = '<p style="color: rgba(255,0,0,.6);font-weight: 1000; ">Finish</p>'
+        submit.innerHTML = '<p style="color: rgba(255,0,0,.6);font-weight: 1000; ">Finish</p>';
 
     }
 
-    questionCount++
+    questionCount++;
 
-    answers.forEach(item => item.checked = false)
+    answers.forEach(item => item.checked = false);
 
 
     if (questionCount < QuizData.length) {
-        loadQuestion()
+        loadQuestion();
     } else {
         quiz.innerHTML = `
         <h1> scored ${score} / ${QuizData.length} </h1>
         <button class="btn" onclick="location.reload()"> Restart Quiz </button>
-        `
+        `;
         clearInterval(timerInterval);
     }
 
-})
+});
